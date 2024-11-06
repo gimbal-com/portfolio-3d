2ad189ab58257c655672b7fa159ed58e985f7f17
Athen Lee <cupid.jack505@gmail.com>
2024-11-06 12:54:13 -0400

    fix: modify experience content

--- src/constants/index.js
+++ src/constants/index.js
@@ -113,11 +113,10 @@ const experiences = [
     iconBg: "rgb(230, 222, 221)",
     date: "Nov 2022 - Present",
     points: [
-      "Led a team to develop the AAMI Real Estate Website with React and Node.js boosting user engagement by 25%",
-      "Created RESTful APIs for managing property data and enabling real-time updates",
-      "Optimized MongoDB queries and indexing strategies to improve data retrieval speed resulting in a 40% reduction in page load time",
-      "Integrated Stripe API for secure payment processing enhancing user trust and increasing conversion rates for property transactions",
-      "Mentored junior developers on Agile practices and coding standards to improve team efficiency"
+      "Led a team in the design and development of the AAMI Real Estate Website boosting user experience and functionality by 25%",
+      "Optimized MongoDB queries reducing load times and improving overall performance by 40% through modified indexing strategies",
+      "Integrated Stripe API to enable secure payment processing enhancing user trust and increasing transaction conversion rates by 15%",
+      "Mentored junior developers fostering their growth and increasing team efficiency by 20% through Agile practices and coding standards",
     ],
   },
   {
@@ -127,11 +126,10 @@ const experiences = [
     iconBg: "rgb(230, 222, 221)",
     date: "Jan 2022 - Oct 2022",
     points: [
-      "Developed the Cogmento CRM platform using React and Django enhancing user engagement by 30%",
-      "Worked closely with team members to gather requirements from PM and UI/UX designers leading to a 20% decrease in development time",
-      "Created RESTful APIs in Django improving data transfer efficiency and increasing application performance by 25%",
-      "Guided 3 junior developers focusing on best coding practices which helped reduce code review revisions by 40%",
-      "Facilitated weekly sync-ups with the team to track progress resulting in a 15% boost in overall productivity"
+      "Created the Cogmento CRM platform driving a 30% increase in user engagement through innovated UI/UX features and performance",
+      "Partnered with PMs and UI/UX designers to gather requirements resulting in a 20% decrease in development time by refining workflows",
+      "Established and upgraded RESTful APIs improving data transfer efficiency and increasing overall application performance by 25%",
+      "Supported 3 junior developers by sharing best coding conventions and examples which helped reduce code review revisions by 40%",
     ],
   },
   {
@@ -141,10 +139,10 @@ const experiences = [
     iconBg: "rgb(56, 62, 86)",
     date: "Mar 2019 - Jan 2022",
     points: [
-      "Enhanced the Goons of Balatroon NFT card game, leading to a 160% increase in daily active users through backend optimizations",
-      "Developed an NFT management app using Node.js, streamlining operations and achieving a 25% increase in NFT transactions",
-      "Implemented backend integrations for Highcharts to support an interactive player statistics visualization feature, enhancing player engagement duration by 30%",
-      "Collaborated with cross-functional teams as a backend developer, participating in weekly technical reviews and sprint planning to align project goals"
+      "Streamlined the backend of the Goons of Balatroon NFT card game resulting in a 160% increase in the number of daily active users",
+      "Built an NFT management app using Node.js streamlining backend processes and leading to a 25% rise in NFT transactions",
+      "Implemented backend integrations with Highcharts to support interactive player statistics increasing user engagement by 30%",
+      "Collaborated effectively with cross-functional teams leading to a 20% reduction in project delivery time"
     ],
   },
   {
@@ -154,25 +152,24 @@ const experiences = [
     iconBg: "rgb(56, 62, 86)",
     date: "Feb 2017 - Feb 2019",
     points: [
-      "Developed and launched a website for an open-source software company leading 40% increase in website traffic",
-      "Built a Hummingbot Miner app using React and Node.js streamlining the mining process and reducing manual task time by 40%",
-      "Redesigned the frontend on-boarding process in collaboration with product and marketing teams enhancing user experience and retention by 30%",
-      "Implemented analytics tools to track user engagement providing data-driven insights that informed further enhancements"
+      "Launched a website for crypto market makers for an open-source software company resulting in a 40% increase in website traffic",
+      "Redesigned the frontend onboarding process in collaboration with QA and marketing teams improving user retention by 30%",
+      "Implemented analytics tools on the frontend to trak user engagement leading to a 15% increase active users",
     ],
   },
-  {
-    title: "Frontend Developer",
-    company_name: "Properstar",
-    icon: "/company/proper.png",
-    iconBg: "rgb(56, 62, 86)",
-    date: "Mar 2021 - Apr 2022",
-    points: [
-      "Developed robust Andriod and iOS real estate applications using React Native and React Navigation optimizing performance to achieve a 40% reduction in loading times and 50% faster user interactions",
-      "Created a real estate agent platform with React and Node.js enhancing user experience and driving a 20% increase in site traffic",
-      "Revamped the platform by adding advanced search functionality and personalized user dashboards leading to a 50% decrease in property search time",
-      "Utilized D3.js to develop innovative data visualizations improving insights and decision-making for users"
-    ],
-  }
+  // {
+  //   title: "Frontend Developer",
+  //   company_name: "Properstar",
+  //   icon: "/company/proper.png",
+  //   iconBg: "rgb(56, 62, 86)",
+  //   date: "Mar 2021 - Apr 2022",
+  //   points: [
+  //     // "Developed robust Andriod and iOS real estate applications using React Native and React Navigation optimizing performance to achieve a 40% reduction in loading times and 50% faster user interactions",
+  //     // "Created a real estate agent platform with React and Node.js enhancing user experience and driving a 20% increase in site traffic",
+  //     // "Revamped the platform by adding advanced search functionality and personalized user dashboards leading to a 50% decrease in property search time",
+  //     // "Utilized D3.js to develop innovative data visualizations improving insights and decision-making for users"
+  //   ],
+  // }
 
 ];
 
@@ -314,23 +311,23 @@ const projects = [
     image: diyar,
     site_link: "https://www.diyar.bh",
   },
-  {
-    name: "Traveling Girls",
-    description:
-    "TravelGirls is a travel companion website that connects men with women looking for travel partners, focusing on romantic connections and shared travel experiences.",
-      tags: [
-        {
-          name: "Angular",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "Node.js",
-          color: "green-text-gradient",
-        }
-      ],
-    image: travel,
-    site_link: "https://www.travelgirls.com",
-  },
+  // {
+  //   name: "Traveling Girls",
+  //   description:
+  //   "TravelGirls is a travel companion website that connects men with women looking for travel partners, focusing on romantic connections and shared travel experiences.",
+  //     tags: [
+  //       {
+  //         name: "Angular",
+  //         color: "blue-text-gradient",
+  //       },
+  //       {
+  //         name: "Node.js",
+  //         color: "green-text-gradient",
+  //       }
+  //     ],
+  //   image: travel,
+  //   site_link: "https://www.travelgirls.com",
+  // },
   {
     name: "Vikids Women",
     description:
