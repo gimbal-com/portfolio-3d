4fdf7c7ae536500be6d5dc359779237e76bdb6f5
David <34836882+gimbal-com@users.noreply.github.com>
2024-11-11 17:01:51 -0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF] text-[70px] ">Arved</span>
+            Hi, I'm <span className="text-[#915EFF] text-[70px] ">David Leadley</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I transform challenges <br className="sm:block hidden" />
